f1e91ed769b7c4d6efebe1e90fba8a68039b4ee9
Grigorij Tckachencko <tckachencko.grigorij@yandex.ru>
2023-05-02 15:02:22 +0300

    url methods are replaced into new url module for more readability. Add documentation.

--- cryptocurency analyzer/coins.py
+++ cryptocurency analyzer/coins.py
@@ -1,6 +1,8 @@
-import asyncio
-import requests
-from tokencolector import TokenColector
+"""
+ Collecting the data from the AlphaVantage API
+"""
+
+import aiohttp
 
 
 class DataCollector():
@@ -10,15 +12,29 @@ class DataCollector():
     Actual prices of crypto coins
     """    """"""
 
-    def __init__(self, coin, api) -> None:
+    def __init__(self, base_coin, secondary_coin, api_token) -> None:
         """Constructor of the DataCollector object
         Args: coin - is the actual coin name"""
 
-        self.coin = coin
-        self.api = api
+        self.base_coin = base_coin
+        self.secondary_coin = secondary_coin
+        self.api_token = api_token
 
-    
-    def get_coin_price(self):
-        ...
+    async def get_coin_price(self, url: str) -> dict:
+        """
+        get_coin_price Asyncio method
 
-api_key = TokenColector(token_name="VANTAGE", file_name="./API.env")
+        use it only with asyncio.run()
+
+        Args:
+            url (str): url of the api you have chosen
+
+        Returns:
+            dict: {response: response_status,
+                   output: text_formated_output}
+        """
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                status_code = response.status
+                output_api = await response.json()
+        return {"status": status_code, "output": output_api}
